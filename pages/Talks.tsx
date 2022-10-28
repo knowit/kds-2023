22695de669da8226f08204f0a8f08f4ebd9976e3
Johnny Bjånesøy <johnny.bjanesoy@knowit.no>
2022-10-28 11:06:32 +0200

    Make talk fetching responsive

--- pages/Talks.tsx
+++ pages/Talks.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Button,
+  CircularProgress,
   Divider,
   Grid,
   IconButton,
@@ -10,30 +11,57 @@ import {
 import { NextPage } from 'next'
 import { useContext, useEffect, useState } from 'react'
 import directus from '../lib/directus'
-import { DateTime } from 'luxon'
 import { DirectusConference, DirectusTimeslot } from '../src/types'
 import CloseIcon from '@mui/icons-material/Close'
 import { BackgroundContext } from '../src/components/layout'
 import { knowitPear } from '../src/styles/colors'
 import { TalkOverview } from '../src/components/modules'
-interface TalkPageProps {
-  talks: DirectusTimeslot[]
-  events: DirectusTimeslot[]
-}
 
-const Talks: NextPage<TalkPageProps> = ({ talks, events }) => {
-  useEffect(() => {
-    console.log('banana', talks)
-  }, [talks, events])
+const Talks: NextPage = () => {
+  //Set Background
   const [_, setBackgroundUsed] = useContext(BackgroundContext)
-
   useEffect(() => {
     setBackgroundUsed('soft1')
   }, [])
 
-  const [tagFilters, setTagFilters] = useState<string[]>([])
+  //Populate Talks on pageload
+  const [talks, setTalks] = useState<DirectusTimeslot[]>()
 
-  const [filteredTalks, setFilteredTalks] = useState<DirectusTimeslot[]>(talks)
+  const getTalks = async () => {
+    const res = await directus.items('Conference').readByQuery({
+      limit: 1,
+      fields: [
+        'Name',
+        'Timeslots.Type',
+        'Timeslots.StartTime',
+        'Timeslots.Talk.*.*',
+        'Timeslots.Talk.Speakers.Speaker_SpeakerId.CompanyName',
+        'Timeslots.Talk.Speakers.Speaker_SpeakerId.Description',
+        'Timeslots.Talk.Speakers.Speaker_SpeakerId.SpeakerId',
+        'Timeslots.Talk.Speakers.Speaker_SpeakerId.SpeakerName',
+      ],
+      filter: {
+        Name: {
+          _eq: 'KDS 2023',
+        },
+      },
+    })
+    const currentConference: DirectusConference = res.data?.find(
+      (conference) => conference
+    )
+    const fetchedTalks = currentConference.Timeslots.filter(
+      (Timeslot) => Timeslot.Type == 'talk'
+    )
+    setTalks(fetchedTalks.length == 0 ? [] : fetchedTalks)
+  }
+
+  useEffect(() => {
+    getTalks()
+  }, [])
+
+  //Handle tag filtering
+  const [tagFilters, setTagFilters] = useState<string[]>([])
+  const [filteredTalks, setFilteredTalks] = useState<DirectusTimeslot[]>([])
 
   const handleTagClick = (tag: string) => {
     if (tagFilters?.includes(tag)) {
@@ -51,33 +79,46 @@ const Talks: NextPage<TalkPageProps> = ({ talks, events }) => {
   }
 
   useEffect(() => {
-    if (tagFilters.length < 1) {
-      setFilteredTalks(talks)
-    } else {
-      setFilteredTalks(
-        talks.filter((timeslot) =>
-          timeslot.Talk.Tags?.some((tag) => tagFilters.includes(tag))
+    if (talks) {
+      if (tagFilters.length < 1) {
+        setFilteredTalks(talks)
+      } else {
+        setFilteredTalks(
+          talks.filter((timeslot) =>
+            timeslot.Talk.Tags?.some((tag) => tagFilters.includes(tag))
+          )
         )
-      )
+      }
     }
   }, [tagFilters])
-  DateTime
+
+  useEffect(() => {
+    if (talks && talks.length > 0) setFilteredTalks(talks)
+  }, [talks])
+
   return (
     <Grid container rowSpacing={2} justifyContent='center' alignItems='center'>
       <Grid item container justifyContent='center' alignItems='center' xs={12}>
         <Paper variant='transparent' sx={{ maxWidth: '1000px' }}>
           <Grid item container>
-            <Grid item>
-              <Typography variant='h3'>{'Talks [WORK IN PROGRESS]'}</Typography>
+            <Grid item container justifyContent={'center'}>
+              <Typography variant='h3'>{'Talks'}</Typography>
             </Grid>
             <Grid item>
               <Typography>
                 Welcome to Knowit Developer summit! Let yourself be inspired to
                 attend this year by browsing the talks submitted to our
                 conference. Timeslots and programs will be published at a later
-                date. If you are presenting and wish to make some changes to the
-                talk, please send a mail to kds@knowit.no
+                date.
               </Typography>
+              <Grid item>
+                <Typography>
+                  If you are a presentor and wish to make any changes to your
+                  presentation info, contact <u>johnny.bjanesoy@knowit.no </u>.
+                  For cancellations or other inquiries, contact{' '}
+                  <u>kds@knowit.no</u>
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
         </Paper>
@@ -100,66 +141,36 @@ const Talks: NextPage<TalkPageProps> = ({ talks, events }) => {
         </Paper>
       </Grid>
       <Grid item container>
-        {filteredTalks?.map((timeslot, idx) => (
-          <Grid
-            item
-            container
-            key={idx}
-            xs={12}
-            alignItems='center'
-            justifyContent='center'
-          >
-            <TalkOverview
-              timeslot={timeslot}
-              tagFilters={tagFilters}
-              handleTagClick={handleTagClick}
-            />
-            <Box width={'50%'} margin={'2rem 0rem'}>
-              <Divider color={knowitPear} />
-            </Box>
+        {talks === undefined ? (
+          <Grid item container justifyContent='center'>
+            <CircularProgress size='6rem' color={'secondary'} />
           </Grid>
-        ))}
+        ) : talks.length == 0 ? (
+          <Typography>Error retrieving talks from Directus</Typography>
+        ) : (
+          filteredTalks?.map((timeslot, idx) => (
+            <Grid
+              item
+              container
+              key={idx}
+              xs={12}
+              alignItems='center'
+              justifyContent='center'
+            >
+              <TalkOverview
+                timeslot={timeslot}
+                tagFilters={tagFilters}
+                handleTagClick={handleTagClick}
+              />
+              <Box width={'50%'} margin={'2rem 0rem'}>
+                <Divider color={knowitPear} />
+              </Box>
+            </Grid>
+          ))
+        )}
       </Grid>
     </Grid>
   )
-}
-
-export const getStaticProps = async () => {
-  const res = await directus.items('Conference').readByQuery({
-    limit: 1,
-    fields: [
-      'Name',
-      'Timeslots.Type',
-      'Timeslots.StartTime',
-      'Timeslots.Talk.*.*',
-      'Timeslots.Talk.Speakers.Speaker_SpeakerId.CompanyName',
-      'Timeslots.Talk.Speakers.Speaker_SpeakerId.Description',
-      'Timeslots.Talk.Speakers.Speaker_SpeakerId.SpeakerId',
-      'Timeslots.Talk.Speakers.Speaker_SpeakerId.SpeakerName',
-    ],
-    filter: {
-      Name: {
-        _eq: 'KDS 2023',
-      },
-    },
-  })
-  const currentConference: DirectusConference = res.data?.find(
-    (conference) => conference
-  )
-  const happenings: TalkPageProps = {
-    talks: currentConference.Timeslots.filter(
-      (Timeslot) => Timeslot.Type == 'talk'
-    ),
-    events: currentConference.Timeslots.filter(
-      (Timeslot) => Timeslot.Type == 'event'
-    ),
-  }
-  return {
-    props: {
-      talks: happenings.talks,
-      events: happenings.events,
-    },
-  }
 }
 
 export default Talks
